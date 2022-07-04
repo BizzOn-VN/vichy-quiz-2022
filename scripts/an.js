bbee13c1b50051da9dfbc628d94d906a5e8a6732
an <an0209vn@gmail.com>
2022-07-04 17:23:09 +0700

    udate html
    udate html

--- scripts/an.js
+++ scripts/an.js
@@ -55,10 +55,47 @@ $('.ui.checkbox').checkbox();
      		$(data_src).addClass("active-section");
      	})
      },
+     cta_slide_question:function(){
+     	$(".data-slide-question").click(function(){
+     		var data_slide_question=$(this).attr("data-slide-question");
+     		$(this).parents(".blk-quizz").find(".pakage-faq").removeClass("active-pakage-faq");
+     		$(data_slide_question).addClass("active-pakage-faq");
+
+
+     		$('.skillbar').each(function(){
+     			jQuery(this).find('.skillbar-bar').animate({
+     				width:jQuery(this).attr('data-percent')
+     			},900);
+     		});
+     		$('.count').each(function () {
+     		    $(this).prop('Counter',0).animate({
+     		        Counter: $(this).text()
+     		    }, {
+     		        duration: 900,
+     		        easing: 'swing',
+     		        step: function (now) {
+     		            $(this).text(Math.ceil(now));
+     		        }
+     		    });
+     		});
+     	})
+     },
+     data_question:function(){
+     	$(".data-question").click(function(){
+     		var data_question=$(this).attr("data-question");
+     		$(this).parents("section").find(".ico-next").attr("data-src",data_question);
+     		$(".prev-result").attr("data-src",data_question);
+
+
+     		var data_result=$(this).attr("data-result");
+     		$(this).closest(".page-main").find(".result-item").removeClass("active-result-item");
+     		$(data_result).addClass("active-result-item");
+     	})
+     },
     progress_bar:function(){
     	$(".active-progress-bar").click(function(){
-    		if($(this).hasClass("active")){
-    		}else{
+    		// if($(this).hasClass("active")){
+    		// }else{
     			$('.skillbar').each(function(){
     				jQuery(this).find('.skillbar-bar').animate({
     					width:jQuery(this).attr('data-percent')
@@ -75,8 +112,8 @@ $('.ui.checkbox').checkbox();
     			        }
     			    });
     			});
-    		}
-    		$(this).addClass("active");
+    		// }
+    		// $(this).addClass("active");
     	})
     }
 
@@ -86,6 +123,8 @@ $('.ui.checkbox').checkbox();
      App. set_min_height_section();
      App. cta_link(); 
      App. progress_bar(); 
+     App. data_question();
+     App. cta_slide_question(); 
      	jQuery(window).resize(function () {
        		App. set_min_height_section();   
      	});
